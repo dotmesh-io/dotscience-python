8d9f379d96589108047f12119d7902486a48e19d
Alaric Snell-Pym <alaric@dotmesh.com>
2018-10-19 14:45:05 +0100

    dotscience#1: CI test and build

--- shipit-pypi.sh
+++ shipit-pypi.sh
@@ -16,4 +16,9 @@ docker build -t $BASE -f Dockerfile.base .
 
 echo "### Shipping package to PyPi"
 
-docker run $BASE /bin/bash -c "pip install twine ; cd dsbuild ; python3 setup.py sdist bdist_wheel ; twine upload -u $PYPI_USER -p $PYPI_PASSWORD --repository-url $PYPI_REPO dist/*"
+if [ -n "$PYPI_REPO" ]
+then
+    REPO_ARG="--repository-url $PYPI_REPO"
+fi
+
+docker run $BASE /bin/bash -c "pip install twine ; cd dsbuild ; python3 setup.py sdist bdist_wheel ; twine upload -u $PYPI_USER -p $PYPI_PASSWORD $REPO_ARG dist/*"
