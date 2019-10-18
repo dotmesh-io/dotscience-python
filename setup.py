f133aa6d9c7933c02a1d8f662836268e7d7bea8d
Charlotte Godley <charlotte@dotmesh.com>
2019-10-18 10:38:32 +0100

    dotmesh-io/python-sdk#8: remove unneeded jsonrpc libs

--- setup.py
+++ setup.py
@@ -15,8 +15,8 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/dotmesh-io/dotscience-python",
     packages=setuptools.find_packages(),
-    install_requires=['jsonrpcclient[requests]', 'requests', 'datadots-api>=0.2.1'],
-    tests_require=['pytest', 'hypothesis', 'jsonrpcclient[requests]', 'requests', 'datadots-api>=0.2.1'],
+    install_requires=['datadots-api>=0.2.1'],
+    tests_require=['pytest', 'hypothesis', 'datadots-api>=0.2.1'],
     zip_safe=True,
     classifiers=[
         "Programming Language :: Python :: 3",
