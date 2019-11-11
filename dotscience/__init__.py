71f5a9eac6fcbb50adee9a572da75e952e5f5f7f
Karolis Rusenas <karolis.rusenas@gmail.com>
2019-11-11 17:04:46 +0000

    Bump version: 0.6.9 → 0.7.0

--- dotscience/__init__.py
+++ dotscience/__init__.py
@@ -1,5 +1,5 @@
 name = "dotscience"
-__version__ = '0.6.9'
+__version__ = '0.7.0'
 
 import json
 import datetime
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="dotscience",
-    version="0.6.9",
+    version="0.7.0",
     author="Subtree, Inc",
     author_email="support@dotscience.com",
     description="Tools for writing Dotscience workloads",
