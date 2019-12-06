238dd7527a87c6b1a7ca0e4c3756cc52a1c7401d
Priya Samuel <priya@dotmesh.io>
2019-12-06 16:15:37 +0000

    Bump version: 0.8.3 → 0.8.4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="dotscience",
-    version="0.8.3",
+    version="0.8.4",
     author="Subtree, Inc",
     author_email="support@dotscience.com",
     description="Tools for writing Dotscience workloads",
