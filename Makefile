fd774c459fd654380f7d6d299bf7a7f642694d8c
Itamar Turner-Trauring <itamar@itamarst.org>
2020-04-28 14:50:07 -0400

    Start infrastructure for paired .py/.ipynb prediction templates.

--- Makefile
+++ Makefile
@@ -1,8 +1,14 @@
-install-local:
-	python setup.py sdist
-	pip3 install dist/dotscience-*.tar.gz 
+.PHONY: generate-ipynb
+generate-ipynb:
+	jupytext --to notebook dotscience/files/*.predict.py
 
-test:
+.PHONY: install-local
+install-local: generate-ipynb
+	python setup.py sdist
+	pip3 install dist/dotscience-*.tar.gz
+
+.PHONY: test
+test: generate-ipynb
 	pip3 install --upgrade pip setuptools wheel pytest hypothesis
 	pip3 install .
 	pytest dotscience
--- setup.py
+++ setup.py
@@ -1,7 +1,24 @@
 import setuptools
+import glob
+import os
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
+
+def ensure_ipynb():
+    """
+    Make sure all generated .ipynb are newer than their corresponding source .py
+    file.
+    """
+    for py in glob.glob("dotscience/files/*.predict.py"):
+        ipynb = py[:-2] + "ipynb"
+        if not os.path.exists():
+            return False
+        if os.stat(py).mtime >= os.stat(ipynb).mtime:
+            return False
+    return True
+if not ensure_ipynb():
+    raise SystemExit("Notebooks out of date, please run: make generate-ipynb")
 
 setuptools.setup(
     name="dotscience",
